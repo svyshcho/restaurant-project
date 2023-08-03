8390ce32df94c1ff08a09b405701f487a3d789f1
Illya Svyshcho <iill44iikk@gmail.com>
2023-08-03 23:31:32 +0300

    Added models to Admin page

--- restaurant/models.py
+++ restaurant/models.py
@@ -10,7 +10,7 @@ class DishType(models.Model):
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(default=1)
 
     def __str__(self):
         return f"{self.username}, ({self.first_name} {self.last_name})"
