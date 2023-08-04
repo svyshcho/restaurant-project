a5f9c7f4ff17fb54674301da1d3988d05ebcbec8
Illya Svyshcho <iill44iikk@gmail.com>
2023-08-04 20:27:43 +0300

    Added list page for dishes

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from restaurant.views import index
+from restaurant.views import index, FoodListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("category/<int:pk>", FoodListView.as_view(), name="food-list"),
 ]
 
 app_name = "restaurant"
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from restaurant.models import DishType, Dish, Cook
 
 
@@ -18,4 +18,24 @@ def index(request):
     }
 
     return render(request, "restaurant/index.html", context=context)
+
+
+class FoodListView(generic.ListView):
+    model = Dish
+    context_object_name = "dishes_list"
+    template_name = "restaurant/dishes_list.html"
+
+    def get_queryset(self):
+        dish_type = self.kwargs["pk"]
+        queryset = Dish.objects.filter(dish_type__id=dish_type)
+
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        dish_pk = self.kwargs["pk"]
+        dish_type = get_object_or_404(DishType, pk=dish_pk)
+        context["dish_type"] = dish_type
+        context["dish_type_list"] = DishType.objects.all()
+        return context
 
