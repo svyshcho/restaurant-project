76c8659ed5f3fc2fefc3ca846865a170c99bb358
Illya Svyshcho <iill44iikk@gmail.com>
2023-08-04 15:23:23 +0300

    Added sidebar

--- restaurant/views.py
+++ restaurant/views.py
@@ -7,11 +7,14 @@ def index(request):
     num_dish_types = DishType.objects.count()
     num_dishes = Dish.objects.count()
     num_cookers = Cook.objects.count()
+    dish_type_list = DishType.objects.all()
 
     context = {
         "num_dish_types": num_dish_types,
         "num_dishes": num_dishes,
         "num_cookers": num_cookers,
+        "dish_type_list": dish_type_list
+
     }
 
     return render(request, "restaurant/index.html", context=context)
